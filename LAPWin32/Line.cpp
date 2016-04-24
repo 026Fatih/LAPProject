6d0aa330c188b035279b533369bee9a867f77b15
Fatih Demir <fatihdemir94@ymail.com>
2016-04-24 18:08:56 +0300

    Alternatif Line constructor

--- LAPWin32/Line.cpp
+++ LAPWin32/Line.cpp
@@ -6,6 +6,15 @@ Line::Line(void)
 {
 }
 
+Line::Line(int xS, int yS, int zS, int xE, int yE, int zE)
+{
+	this->startPoint.x = xS;
+	this->startPoint.y = yS;
+	this->startPoint.z = zS;
+	this->endPoint.x = xE;
+	this->endPoint.x = yE;
+	this->endPoint.x = zE;
+}
 
 Line::~Line(void)
 {
--- LAPWin32/Line.h
+++ LAPWin32/Line.h
@@ -1,11 +1,15 @@
 #pragma once
 #include "drawable.h"
+#include "Point.h"
+
 class Line :
 	public Drawable
 {
+private:
+	Point startPoint, endPoint;
 public:
 	Line(void);
-
+	Line(int xS, int yS, int zS, int xE, int yE, int zE);
 	virtual void draw();
 
 	~Line(void);

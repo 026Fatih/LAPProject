6d83a847ca6720b1655e956a34702a4a2210c082
Fatih Demir <fatihdemir94@ymail.com>
2016-04-25 23:20:16 +0300

    Quadrilateral objesini oluşturan Line objeleri ve alternatif constructor

--- LAPWin32/Quadrilateral.cpp
+++ LAPWin32/Quadrilateral.cpp
@@ -6,6 +6,14 @@ Quadrilateral::Quadrilateral(void)
 {
 }
 
+Quadrilateral::Quadrilateral(int x1, int y1, int z1, int x2, int y2, int z2,
+	int x3, int y3, int z3, int x4, int y4, int z4)
+{
+	lines[0] = new Line(x1, y1, z1, x2, y2, z2);
+	lines[1] = new Line(x2, y2, z2, x3, y3, z3);
+	lines[2] = new Line(x3, y3, z3, z4, y4, z4);
+	lines[3] = new Line(x4, y4, z4, x1, y1, z1);
+}
 
 Quadrilateral::~Quadrilateral(void)
 {

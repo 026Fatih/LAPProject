9489fdb79aa575122a878a19b6ca20f59090a447
Fatih Demir <fatihdemir94@ymail.com>
2016-04-25 23:56:52 +0300

    Yazım hatası

--- LAPWin32/Quadrilateral.cpp
+++ LAPWin32/Quadrilateral.cpp
@@ -11,7 +11,7 @@ Quadrilateral::Quadrilateral(int x1, int y1, int z1, int x2, int y2, int z2,
 {
 	lines[0] = new Line(x1, y1, z1, x2, y2, z2);
 	lines[1] = new Line(x2, y2, z2, x3, y3, z3);
-	lines[2] = new Line(x3, y3, z3, z4, y4, z4);
+	lines[2] = new Line(x3, y3, z3, x4, y4, z4);
 	lines[3] = new Line(x4, y4, z4, x1, y1, z1);
 }
 

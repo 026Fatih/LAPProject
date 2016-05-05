d494a35728e82a9368640b697237799ece49494e
Fatih Demir <fatihdemir94@ymail.com>
2016-05-05 22:23:26 +0300

    BezierCurve alternatif constructor

--- LAPWin32/BezierCurve.cpp
+++ LAPWin32/BezierCurve.cpp
@@ -6,6 +6,15 @@ BezierCurve::BezierCurve(void)
 {
 }
 
+BezierCurve::BezierCurve(int x1, int y1, int z1, int x2, int y2, int z2, int x3, int y3, int z3)
+{
+
+}
+
+void BezierCurve::draw()
+{
+
+}
 
 BezierCurve::~BezierCurve(void)
 {
--- LAPWin32/BezierCurve.h
+++ LAPWin32/BezierCurve.h
@@ -5,6 +5,10 @@ class BezierCurve :
 {
 public:
 	BezierCurve(void);
+	BezierCurve(int x1, int y1, int z1, int x2, int y2, int z2, int x3, int y3, int z3);
+
+	virtual void draw();
+
 	~BezierCurve(void);
 };
 

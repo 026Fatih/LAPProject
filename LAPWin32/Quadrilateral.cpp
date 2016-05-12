f3eb8d0295c380b8ab987c6ed38bc945c7aa6828
Nilsu Vardaraeri <nilsuvardareri@gmail.com>
2016-05-12 14:45:39 +0300

    Mesh function

--- LAPWin32/Quadrilateral.cpp
+++ LAPWin32/Quadrilateral.cpp
@@ -31,3 +31,51 @@ Quadrilateral::~Quadrilateral(void)
 	for (int i = 0; i < 4; i++)
 		delete lines[i];
 }
+
+std::vector<Line*> Quadrilateral::mesh()
+{
+	std::vector<Line*> meshLines;
+
+	int distX0=lines[0]->startPoint.x-lines[0]->endPoint.x;
+	int distY0=lines[0]->startPoint.y-lines[0]->endPoint.y;
+	int distZ0=lines[0]->startPoint.z-lines[0]->endPoint.z;
+
+	int distX2=lines[2]->startPoint.x-lines[2]->endPoint.x;
+	int distY2=lines[2]->startPoint.y-lines[2]->endPoint.y;
+	int distZ2=lines[2]->startPoint.z-lines[2]->endPoint.z;
+
+	int distX1=lines[1]->startPoint.x-lines[1]->endPoint.x;
+	int distY1=lines[1]->startPoint.y-lines[1]->endPoint.y;
+	int distZ1=lines[1]->startPoint.z-lines[1]->endPoint.z;
+
+	int distX3=lines[3]->startPoint.x-lines[3]->endPoint.x;
+	int distY3=lines[3]->startPoint.y-lines[3]->endPoint.y;
+	int distZ3=lines[3]->startPoint.z-lines[3]->endPoint.z;
+
+	for(int i=1;i<11;i++)
+	{int tempX=lines[0]->startPoint.x+distX0/10*i;
+	int tempY=lines[0]->startPoint.y+distY0/10*i;
+	int tempZ=lines[0]->startPoint.z+distZ0/10*i;
+
+
+	int tempX2=lines[2]->startPoint.x+distX2/10*i;
+	int tempY2=lines[2]->startPoint.y+distY2/10*i;
+	int tempZ2=lines[2]->startPoint.z+distZ2/10*i;
+
+	meshLines.push_back(new Line(tempX,tempY,tempZ,tempX2,tempY2,tempZ2));
+
+	int tempX1=lines[1]->startPoint.x+distX1/10*i;
+	int tempY1=lines[1]->startPoint.y+distY1/10*i;
+	int tempZ1=lines[1]->startPoint.z+distZ1/10*i;
+
+
+	int tempX3=lines[3]->startPoint.x+distX3/10*i;
+	int tempY3=lines[3]->startPoint.y+distY3/10*i;
+	int tempZ3=lines[3]->startPoint.z+distZ3/10*i;
+
+	meshLines.push_back(new Line(tempX1,tempY1,tempZ1,tempX3,tempY3,tempZ3));
+
+
+	}
+
+}
--- LAPWin32/Quadrilateral.h
+++ LAPWin32/Quadrilateral.h
@@ -14,4 +14,5 @@ public:
 	virtual void draw();
 
 	~Quadrilateral(void);
+	std::vector<Line*> mesh();
 };
